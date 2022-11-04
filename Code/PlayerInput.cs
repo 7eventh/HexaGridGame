4e57529bb28f5c5503d3d23d597b88b1a5e61a52
7eventh <55671529+7eventh@users.noreply.github.com>
2022-11-04 13:57:29 +0100

    Update PlayerInput.cs

--- Code/PlayerInput.cs
+++ Code/PlayerInput.cs
@@ -5,6 +5,9 @@ using UnityEngine.Events;
 
 public class PlayerInput : MonoBehaviour
 {
+    [SerializeField] private string Ostcl = "Obstacle";
+    [SerializeField] private string Empt = "Empty";
+
     public UnityEvent<Vector3> PointerClick;
 
     void Update()
@@ -14,10 +17,34 @@ public class PlayerInput : MonoBehaviour
 
     private void DetectMouseClick()
     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        
+        // Get mouse position Only when there is a click 
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mousePos = Input.mousePosition;
             PointerClick?.Invoke(mousePos);
         }
+
+        // Get information about object by shooting rays 
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            var selection = hitInfo.transform;
+            // IF player clicks a hex compare the tags.
+            // Depending on if the hax is empty do something.
+            if (Input.GetMouseButtonDown(0))
+            {   
+                if (selection.CompareTag(Ostcl))
+                {
+                    Debug.Log("Has Obstacle");   
+                }
+                // If is empty allow player to place a trap. (If available)
+                else if (selection.CompareTag(Empt))
+                {
+                    Debug.Log("Empty");
+                }
+            }
+        }   
     }
 }
